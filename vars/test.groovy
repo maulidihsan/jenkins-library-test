78f72094fce9405e5b01fb91f90f87550fa1377f
Maulid Ihsan <maulid.ihsan@gmail.com>
2019-08-22 11:28:29 +0700

    Create test.groovy

---
+++ vars/test.groovy
@@ -0,0 +1,24 @@
+pipeline {
+    agent {
+        node {
+            label 'jenkins'
+        }
+    }
+    stages {
+        stage('Build') {
+            steps {
+                sh 'mvn -B -DskipTests clean package'
+            }
+        }
+        stage('Test') {
+            steps {
+                sh 'mvn test'
+            }
+        }
+        stage('Deliver') {
+            steps {
+                sh './jenkins/scripts/deliver.sh'
+            }
+        }
+    }
+}
